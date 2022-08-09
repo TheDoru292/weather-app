64d9dcd9634eda501744fdccdae8612e9308c8d1
TheDoru <thedoruv2@gmail.com>
2022-08-09 12:52:28 +0200

    Organized code, added show error in app

--- src/domHandling.js
+++ src/domHandling.js
@@ -3,116 +3,128 @@ import { fetchLocation, getCountryName, getLocationTime } from "./weather";
 let json;
 
 function initialization() {
-    getLocation("London")
-    addEventToInput();
+  getLocation("London");
+  addEventToInput();
 }
 
 function addEventToInput() {
-    const form = document.querySelector("form");
-    const search = document.querySelector("#search")
+  const form = document.querySelector("form");
+  const search = document.querySelector("#search");
 
-    form.addEventListener("submit", e => {
-        getLocation(search.value);
-        e.preventDefault();
-    })
+  form.addEventListener("submit", (e) => {
+    getLocation(search.value);
+    e.preventDefault();
+  });
 
-    search.addEventListener('invalid', e => {       
-        if(search.validity.valueMissing) {
-            search.setCustomValidity("Location must not be empty!");
-        }
-    })
+  search.addEventListener("invalid", (e) => {
+    if (search.validity.valueMissing) {
+      search.setCustomValidity("Location must not be empty!");
+    }
+  });
 }
 
 async function getLocation(location) {
-    json = await fetchLocation(location);
+  json = await fetchLocation(location);
 
+  if (typeof json === "object") {
     createPageContent();
     getLocationTime(json.sys.country);
+  } else if (typeof json === "number") {
+    throwError(json);
+  }
+}
+
+function throwError(code) {
+  const container = document.querySelector(".error");
+
+  const errorMessage = document.createElement("p");
+  errorMessage.textContent = `Error code: ${code}`;
+
+  container.append(errorMessage);
+
+  setTimeout(() => {
+    errorMessage.remove();
+  }, 6000);
 }
 
 function createPageContent() {
-    const date = getLocationTime(json.sys.country);
+  const date = getLocationTime(json.sys.country);
 
-    const divContainer = document.querySelector('.container');
-    checkForTime(date[1], divContainer);
+  const divContainer = document.querySelector(".container");
+  checkForTime(date[1], divContainer);
 
-    const container = document.querySelector(".info");
-    container.textContent = '';
+  const container = document.querySelector(".info");
+  container.textContent = "";
 
-    const basicContainer = document.createElement('div');
-    basicContainer.className = 'basic';
+  const basicContainer = document.createElement("div");
+  basicContainer.className = "basic";
 
-    const location = document.createElement("h1");
-    location.textContent = `${json.name}, ${getCountryName(json.sys.country)}`;
+  const location = document.createElement("h1");
+  location.textContent = `${json.name}, ${getCountryName(json.sys.country)}`;
 
-    const status = document.createElement("h2");
-    status.textContent = `${json.weather[0].description}`;
+  const status = document.createElement("h2");
+  status.textContent = `${json.weather[0].description}`;
 
-    const dateContainer = document.createElement('div');
-    dateContainer.className = 'date-container';
+  const dateContainer = document.createElement("div");
+  dateContainer.className = "date-container";
 
-    const dateAndDay = document.createElement('p');
-    dateAndDay.textContent = date[0];
+  const dateAndDay = document.createElement("p");
+  dateAndDay.textContent = date[0];
 
-    const time = document.createElement('p');
-    time.textContent = date[1];
+  const time = document.createElement("p");
+  time.textContent = date[1];
 
-    const temperature = document.createElement("p");
-    temperature.className = 'temp';
-    temperature.textContent = `${json.main.temp}`;
+  const temperature = document.createElement("p");
+  temperature.className = "temp";
+  temperature.textContent = `${json.main.temp}`;
 
-    const sup = document.createElement('sup');
-    sup.textContent = '°C';
+  const sup = document.createElement("sup");
+  sup.textContent = "°C";
 
-    const otherInfo = document.createElement('div');
-    otherInfo.classList = 'other-info';
+  const otherInfo = document.createElement("div");
+  otherInfo.classList = "other-info";
 
-    const feelsLike = document.createElement("p");
-    feelsLike.className = 'temp-like';
-    feelsLike.textContent = `Feels like: ${json.main.feels_like}`;
+  const feelsLike = document.createElement("p");
+  feelsLike.className = "temp-like";
+  feelsLike.textContent = `Feels like: ${json.main.feels_like}`;
 
-    const humidity = document.createElement("p");
-    humidity.textContent = `Humidity: ${json.main.humidity}%`;
+  const humidity = document.createElement("p");
+  humidity.textContent = `Humidity: ${json.main.humidity}%`;
 
-    const wind = document.createElement("p");
-    wind.textContent = `Wind: ${json.wind.speed} KM`
+  const wind = document.createElement("p");
+  wind.textContent = `Wind: ${json.wind.speed} KM`;
 
-    const feelsLikeContainer = document.createElement('div');
-    feelsLikeContainer.className = 'feel-container';
+  const feelsLikeContainer = document.createElement("div");
+  feelsLikeContainer.className = "feel-container";
 
-    const tempSup = document.createElement('sup');
-    tempSup.textContent = '°C';
+  const tempSup = document.createElement("sup");
+  tempSup.textContent = "°C";
 
-    basicContainer.append(location, status);
-    dateContainer.append(dateAndDay, time);
-    feelsLikeContainer.append(feelsLike, sup)
-    otherInfo.append(feelsLikeContainer, humidity, wind);
-    temperature.appendChild(tempSup);
-    container.append(basicContainer, dateContainer, temperature, otherInfo);
-
-    console.log(json);
+  basicContainer.append(location, status);
+  dateContainer.append(dateAndDay, time);
+  feelsLikeContainer.append(feelsLike, sup);
+  otherInfo.append(feelsLikeContainer, humidity, wind);
+  temperature.appendChild(tempSup);
+  container.append(basicContainer, dateContainer, temperature, otherInfo);
 }
 
 function checkForTime(time, container) {
-    let newTime = time.split(' ');
-    let hour = newTime[1].split(':');
+  let newTime = time.split(" ");
+  let hour = newTime[1].split(":");
 
-    if(hour[0] < 12 && hour[0] > 9 && newTime[2] === "PM" ||
-       hour[0] < 7 && newTime[2] === "AM") {
-        container.style.backgroundImage = "url('../dist/assets/night.jpg')";
-        container.style.color = "white";
-    } else if(hour[0] > 6 && hour[0] < 12 && newTime[2] === "AM") {
-        container.style.backgroundImage = "url('../dist/assets/day.jpg')";
-        container.style.color = 'white';
-    } else if(hour[0] < 9 && hour[0] >= 1 && newTime[2] === "PM") {
-        container.style.backgroundImage = "url('../dist/assets/evening.jpg')";
-        container.style.color = "white";
-    }
-
-    console.log(hour);
+  if (
+    (hour[0] < 12 && hour[0] > 9 && newTime[2] === "PM") ||
+    (hour[0] < 7 && newTime[2] === "AM")
+  ) {
+    container.style.backgroundImage = "url('../dist/assets/night.jpg')";
+    container.style.color = "white";
+  } else if (hour[0] > 6 && hour[0] < 12 && newTime[2] === "AM") {
+    container.style.backgroundImage = "url('../dist/assets/day.jpg')";
+    container.style.color = "white";
+  } else if (hour[0] < 9 && hour[0] >= 1 && newTime[2] === "PM") {
+    container.style.backgroundImage = "url('../dist/assets/evening.jpg')";
+    container.style.color = "white";
+  }
 }
 
-export {
-    initialization,
-    getLocationTime
-}
+export { initialization, getLocationTime };
--- src/weather.js
+++ src/weather.js
@@ -1,65 +1,72 @@
-import { hoursToMilliseconds } from 'date-fns';
+import { hoursToMilliseconds } from "date-fns";
 
 // import { format, parseISO } from 'date-fns';
-const ct = require('countries-and-timezones');
+const ct = require("countries-and-timezones");
 
 let apiKey = "ca588f406b51fb1542b1d900b0a9364e";
 
 async function fetchLocation(query) {
-    try {
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=metric`);
-        let responseJson = await response.json();
-    
-        return responseJson;
-    } catch(err) {
-        console.error(err);
+  try {
+    let response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=metric`
+    );
+
+    if (response.status === 200) {
+      let responseJson = await response.json();
+
+      console.log(response.status);
+
+      return responseJson;
+    } else {
+      return response.status;
     }
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 function getCountryName(countryCode) {
-    return ct.getCountry(countryCode).name;
+  return ct.getCountry(countryCode).name;
 }
 
 function getLocationTime(countryCode) {
-    let locationTimezone = ct.getCountry(countryCode).timezones[0];
+  let locationTimezone = ct.getCountry(countryCode).timezones[0];
 
-    let time = new Date().toLocaleString("en-US", { timeZone: locationTimezone });
+  let time = new Date().toLocaleString("en-US", { timeZone: locationTimezone });
 
-    return dateToString(time);
+  return dateToString(time);
 }
 
 function dateToString(string) {
-    let newString = string.split(',');
-    let date = newString[0].split('/');
-    let time = newString[1].split(':');
-    let PMorAM = time[2].split(' ');
+  let newString = string.split(",");
+  let date = newString[0].split("/");
+  let time = newString[1].split(":");
+  let PMorAM = time[2].split(" ");
 
-    let dateString = `${getDateName(date[0])}, ${date[1]}th ${toMonthName(date[0])} ${date[2]}`;
-    let hourString = `${time[0]}:${time[1]} ${PMorAM[1]}`;
+  let dateString = `${getDateName(date[0])}, ${date[1]}th ${toMonthName(
+    date[0]
+  )} ${date[2]}`;
+  let hourString = `${time[0]}:${time[1]} ${PMorAM[1]}`;
 
-    return [dateString, hourString];
+  return [dateString, hourString];
 }
 
 function toMonthName(monthNumber) {
-    const date = new Date();
-    date.setMonth(monthNumber - 1);
+  const date = new Date();
+  date.setMonth(monthNumber - 1);
 
-    return date.toLocaleDateString("en-US", {
-        month: 'long',
-    });
+  return date.toLocaleDateString("en-US", {
+    month: "long",
+  });
 }
 
 function getDateName(dateStr) {
-    let date = new Date();
-    date.setDate(dateStr);
-    
-    return date.toLocaleDateString("en-US", {
-        weekday: 'long',
-    });
+  let date = new Date();
+  date.setDate(dateStr);
+
+  return date.toLocaleDateString("en-US", {
+    weekday: "long",
+  });
 }
 
-export {
-    fetchLocation,
-    getCountryName,
-    getLocationTime
-}
+export { fetchLocation, getCountryName, getLocationTime };
